2317a1f1163d66184ac5b5eadfe416675503450e
Darshilshah97 <darshilrshah1997@gmail.com>
2024-04-29 20:28:38 -0400

    Refactor - manage state with zustand

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,13 @@
-import { Button, HStack, Heading, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
-import useGenres, { Genre } from '../hooks/useGenres'
+import { Button, HStack, Heading, Image, List, ListItem, Spinner } from '@chakra-ui/react';
+import useGenres from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
+import useGameQueryStore from '../store';
 
-interface Props {
-  onSelectGenre: (genre: Genre) => void;
-  selectedGenreId?: number;
-}
 
-const GenreList = ({onSelectGenre, selectedGenreId}:Props) => {
+const GenreList = () => {
 
+  const selectedGenreId = useGameQueryStore(s => s.gameQuery.genreId);
+  const setSelectedGenreId = useGameQueryStore(s => s.setGenreId);
    const {data, isLoading, error} = useGenres();
 
    if (error) return null;
@@ -20,7 +19,7 @@ const GenreList = ({onSelectGenre, selectedGenreId}:Props) => {
       <List>
         {data?.results.map(genre => <ListItem key={genre.id} paddingY='5px'><HStack>
             <Image boxSize={'32px'} objectFit='cover' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}></Image>
-            <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenreId ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
+            <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenreId ? 'bold' : 'normal'} onClick={() => setSelectedGenreId(genre.id)} fontSize='lg' variant='link'>{genre.name}</Button>
           </HStack></ListItem>)}
       </List>
     </>
